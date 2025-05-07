5d217643e17e4dbe1141e55541c86928b7653c8c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-07 09:25:15 -0300

    refactor: ProdutoService usando o ProdutoDTO

--- src/main/java/br/com/fiap/ms_produto/dto/ProdutoDTO.java
+++ src/main/java/br/com/fiap/ms_produto/dto/ProdutoDTO.java
@@ -7,10 +7,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ProdutoDTO {
     private Long id;
     @NotBlank(message = "Campo requerido!")
